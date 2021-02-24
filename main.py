44414fdfb5310c3a1c0f90d20c490b18ba01d22a
AIG(ムIG) <58285388+AIGrecords@users.noreply.github.com>
2021-02-24 18:45:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from tkinter import *
 
+expression = ""
+
+def press(num):
+  global expression
+  expression = expression + str(num)
+  equation.set(expression)
+  
+ def equalpress():
+   try:
+      global expression
+      total = str(eval(expression))
+      equation.set(total)
+      
+      
+      
+      
+    gui.mainloop()
+      
